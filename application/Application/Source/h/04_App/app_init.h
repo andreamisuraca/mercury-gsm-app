74fcf8bc7d09f0de645ecc38349988c52f61bd80
Andrea Misuraca <misuracandrea@gmail.com>
2022-03-19 15:09:12 +0100

    AnMi: Added template file for init phase

---
+++ application/Application/Source/h/04_App/app_init.h
@@ -0,0 +1,49 @@
+/************************************************************************
+*                          EEPROM Interface                             *
+*************************************************************************
+* FileName:         app_init.h                                          *
+* HW:               Mercury System                                      *
+* Author:           A.Misuraca                                          *
+*                                                                       *
+* Software License Agreement:                                           *
+*                                                                       *
+* THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,     *
+* WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED     *
+* TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           *
+* PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE AUTHOR SHALL NOT,      *
+* IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR            *
+* CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.                     *
+*                                                                       *
+* --------------------------------------------------------------------- *
+* Responsible For This File: Andrea Misuraca                            *
+*                                                                       *
+* --------------------------------------------------------------------- *
+* Author       Date        Version      Comment                         *
+* ---------------------------------------------------------------------	*
+* A.Misuraca   19/03/21    1.0          First release                   *
+************************************************************************/
+
+#ifndef APP_INIT_H
+#define	APP_INIT_H
+
+/************************************************************************
+* Includes
+************************************************************************/
+
+/************************************************************************
+* EXPORTED Defines
+************************************************************************/
+
+/************************************************************************
+* EXPORTED Typedef
+************************************************************************/
+
+/************************************************************************
+* EXPORTED Variables
+************************************************************************/
+
+/************************************************************************
+* EXPORTED Functions
+************************************************************************/
+
+#endif    /* APP_INIT_H */
